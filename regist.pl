3717186cdfc0c1d3f457b92e9e56069fc4d53050
hiragram <y@yura.me>
2016-12-08 19:17:41 +0900

    Initial commit

---
+++ regist.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $current = `pwd`;
+chomp($current);
+my $root = ".";
+
+my $filelist = &get_files($root);
+
+foreach my $file (@$filelist) {
+  my @lines = split("\n", `cat $file`);
+  my $i = 1;
+  foreach my $line (@lines) {
+    if ($line =~ /[rR]egist[^e]/) {
+      print STDERR "$current/$file:$i:1: error: 💢😡\n";
+      exit(2);
+    }
+    $i++;
+  }
+}
+
+sub get_files {
+  my $dir = shift;
+  my $filelist = shift;
+  opendir(DIR, "$dir");
+  my @list = grep /^[^\.]/, readdir DIR;
+  foreach my $file (@list) {
+    if (-d "$dir/$file") {
+      $filelist = &get_files("$dir/$file", $filelist);
+    } else {
+      if ($file =~ /.swift$/) {
+        push @$filelist, "$dir/$file";
+      }
+    }
+  }
+  return $filelist;
+}
